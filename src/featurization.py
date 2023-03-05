6e4ca758c2f1aef04b72abc76002bc2c3e0b4b43
Anarghya Das <anarghya@buffalo.edu>
2023-03-05 00:04:36 -0500

    new features

--- src/featurization.py
+++ src/featurization.py
@@ -6,6 +6,31 @@ import numpy as np
 from tqdm import tqdm
 import pywt
 
+# calculate entropy for numpy 2d array by first converting it to a pro
+
+
+def calculate_entropy_2d(array):
+    flat_matrix = array.ravel()
+    # Get unique values and their frequencies
+    _, counts = np.unique(flat_matrix, return_counts=True)
+    # Calculate probabilities
+    probabilities = counts / flat_matrix.size
+    entropy = scipy.stats.entropy(probabilities)
+    return entropy
+
+
+def calculate_statistics(list_values):
+    n5 = np.nanpercentile(list_values, 5)
+    n25 = np.nanpercentile(list_values, 25)
+    n75 = np.nanpercentile(list_values, 75)
+    n95 = np.nanpercentile(list_values, 95)
+    median = np.nanpercentile(list_values, 50)
+    mean = np.nanmean(list_values)
+    std = np.nanstd(list_values)
+    var = np.nanvar(list_values)
+    rms = np.nanmean(np.sqrt(list_values**2))
+    return [n5, n25, n75, n95, median, mean, std, var, rms]
+
 
 def compress(original, level, threshold_percentage, wavelet):
     wavelet_repr = pywt.wavedec2(
@@ -19,25 +44,40 @@ def compress(original, level, threshold_percentage, wavelet):
     return threshold_coefficients
 
 
-def create_features(raw_data, level, threshold, wavelet, num):
-    features_arr = []
+def extract_coefficients(raw_data, level, threshold, wavelet, num, normalize=False):
+    coefficients_arr = []
     for i in range(raw_data.shape[2]):
         subject_data = raw_data[:, :, i]
         transformed = compress(subject_data, level, threshold, wavelet)
         if num == 0:
             transformed_array = transformed[0]
+        elif num == -1:
+            transformed_array = pywt.coeffs_to_array(
+                transformed)[0]  # all coefficients
         else:
             transformed_array = np.vstack(
                 (transformed[num][0], transformed[num][1], transformed[num][2]))
-        # transformed_array = pywt.coeffs_to_array(transformed)[0] # all coefficients
-        # transformed_array = transformed[0] # approximation coefficients
 
-        features_arr.append(transformed_array.flatten())
+        coefficients_arr.append(transformed_array)
 
-    features_arr = np.array(features_arr)
-    features_arr = (features_arr - features_arr.min()) / \
-        (features_arr.max() - features_arr.min())
-    return features_arr
+    coefficients_arr = np.array(coefficients_arr)
+    if normalize:
+        coefficients_arr = (coefficients_arr - coefficients_arr.min()) / \
+            (coefficients_arr.max() - coefficients_arr.min())
+    return coefficients_arr
+
+
+def extract_features(coefficients):
+    all_features = []
+    for i in range(coefficients.shape[0]):
+        subject_coefficients = coefficients[i, :, :]
+        entropy = calculate_entropy_2d(subject_coefficients)
+        statistics = calculate_statistics(subject_coefficients)
+        features = np.concatenate((statistics, [entropy]))
+        all_features.append(features)
+
+    all_features = np.array(all_features)
+    return all_features
 
 
 def convert_labels(labels):
@@ -74,7 +114,6 @@ if data_name == "mi":
         y_train = scipy.io.loadmat(os.path.join(labels_folder, l))["y_train"]
         y_test = scipy.io.loadmat(os.path.join(labels_folder, l))["y_test"]
         all_labels = np.concatenate((y_train, y_test), axis=1).reshape(-1)
-
 else:
     for l in os.listdir(labels_folder):
         labels = scipy.io.loadmat(os.path.join(labels_folder, l))["perclos"]
@@ -85,15 +124,17 @@ model_data = []
 if data_name == "mi":
     for f in tqdm(os.listdir(input_folder)):
         data = np.load(os.path.join(input_folder, f))
-        features = create_features(data, params["level"],
-                                   params["threshold"], params["wavelet"], params["coefficient_number"])
+        coefficients = extract_coefficients(data, params["level"],
+                                            params["threshold"], params["wavelet"], params["coefficient_number"])
+        features = extract_features(coefficients)
         combined = np.column_stack((features, all_labels))
         model_data.append(combined)
 else:
     for i, f in enumerate(tqdm(os.listdir(input_folder))):
         data = np.load(os.path.join(input_folder, f))
-        features = create_features(data, params["level"],
-                                   params["threshold"], params["wavelet"], params["coefficient_number"])
+        coefficients = extract_coefficients(data, params["level"],
+                                            params["threshold"], params["wavelet"], params["coefficient_number"])
+        features = extract_features(coefficients)
         combined = np.column_stack((features, all_labels[i]))
         model_data.append(combined)
 
